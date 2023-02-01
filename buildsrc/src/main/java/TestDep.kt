709b9e7eadd570ff14d0d9e0310b3aa672dad180
PhyoThihaWin <phyothiha2580@gmailc.om>
2023-02-01 14:34:11 +0630

    change yaml

--- buildsrc/src/main/java/TestDep.kt
+++
@@ -1,8 +0,0 @@
-//object TestDep {
-//    const val junit = "junit:junit:4.13.2"
-//    const val androidXJunit = "androidx.test.ext:junit:1.1.3"
-//}
-//
-//object Espresso {
-//    const val core = "androidx.test.espresso:espresso-core:3.4.0"
-//}
--- buildsrc/src/main/kotlin/Dependencies.kt
+++ buildsrc/src/main/kotlin/Dependencies.kt
@@ -50,12 +50,3 @@ object Room {
 object DataStoreDep {
     const val datastorePreferences = "androidx.datastore:datastore-preferences:1.0.0"
 }
-
-object TestDep {
-    const val junit = "junit:junit:4.13.2"
-    const val androidXJunit = "androidx.test.ext:junit:1.1.3"
-}
-
-object Espresso {
-    const val core = "androidx.test.espresso:espresso-core:3.4.0"
-}
