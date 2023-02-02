fc5abed2015e6ac16e818f718f44e1c929d1ee82
PhyoThihaWin <phyothiha2580@gmailc.om>
2023-02-02 15:07:33 +0630

    new buildSrc modified

--- buildsrc/build.gradle.kts
+++ buildsrc/build.gradle.kts
@@ -1,9 +1,32 @@
 import org.gradle.kotlin.dsl.`kotlin-dsl`
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     `kotlin-dsl`
+    `kotlin-dsl-precompiled-script-plugins`
 }
 
 repositories {
     mavenCentral()
+}
+
+buildscript {
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.61")
+    }
+}
+
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    languageVersion = "1.3"
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.61")
 }
